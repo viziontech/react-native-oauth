2ba27714013fae95e8b25e3913135a314484bb17
vizion <83540183+viziontech@users.noreply.github.com>
2021-06-11 14:19:59 -0300

    Fix for OAuth 2 scopes

--- ios/OAuthManager/OAuthClient.m
+++ ios/OAuthManager/OAuthClient.m
@@ -1,22 +1,61 @@
 //
-//  OAuthClient.m
+//  OAuth2Client.m
 //  OAuthManager
 //
-//  Created by Ari Lerner on 10/8/16.
+//  Created by Ari Lerner on 10/7/16.
 //  Copyright © 2016 Facebook. All rights reserved.
 //
 
-#import "OAuthClient.h"
+#import "OAuth2Client.h"
+#import "OAuthManager.h"
+#import "DCTAuth.h"
 
-@implementation OAuthClient
+static NSString *TAG = @"OAuth2Client";
+
+@implementation OAuth2Client
 
 - (void) authorizeWithUrl:(NSString *)providerName
-                      url:(NSString *) url
-                      cfg:(NSDictionary *)cfg
-                onSuccess:(AuthManagerCompletionBlock) onSuccess
-                  onError:(AuthManagerErrorBlock) onError
+                           url:(NSString *) url
+                           cfg:(NSDictionary *)cfg
+                     onSuccess:(AuthManagerCompletionBlock) onSuccess
+                       onError:(AuthManagerErrorBlock) onError
 {
-    NSLog(@"Not implemented in here. Wrong class!");
+    if (cfg == nil) {
+        NSError *err = QUICK_ERROR(E_PROVIDER_NOT_CONFIGURED, @"provider not configured");
+        onError(err);
+        return;
+    }
+    
+    DCTOAuth2Account *account = [self getAccount:providerName cfg:cfg];
+    account.callbackURL = [NSURL URLWithString:url];
+    
+    __weak OAuth2Client *client = self;
+    NSLog(@"authorize ----> %@ %@", cfg, account);
+    // authorizeWithClientID
+    [account authenticateWithHandler:^(NSArray *responses, NSError *error) {
+        NSLog(@"authenticateWithHandler: %@", responses);
+        
+        if (error != nil) {
+            NSString *response = ((DCTAuthResponse *)responses[0]).responseDescription;
+            NSError *err = [NSError errorWithDomain:error.domain
+                                               code:error.code
+                                           userInfo:@{@"response": response}];
+            onError(err);
+            return;
+        }
+
+        [client clearPendingAccount];
+        
+        if (!account.authorized) {
+            NSError *err = QUICK_ERROR(E_ACCOUNT_NOT_AUTHORIZED, @"account not authorized");
+            NSLog(@"Account not authorized: %@", account);
+            onError(err);
+            return;
+        }
+        
+        NSLog(@"Success!: %@", account);
+        onSuccess(account);
+    }];
 }
 
 - (void) reauthenticateWithHandler:(NSString *) providerName
@@ -24,43 +63,14 @@
                          onSuccess:(AuthManagerCompletionBlock) onSuccess
                            onError:(AuthManagerErrorBlock) onError
 {
-    NSLog(@"Not implemented in here. Wrong class!");
-}
-
-- (void) cancelAuthentication
-{
-  if (_account != nil) {
-    @try {
-      [_account cancelAuthentication];
-    }
-    @catch (NSException *exception) {
-      NSLog(@"An exception occurred while cancelling authentication: %@", [exception reason]);
-    }
-  }
-}
-
-#pragma mark - Helpers
-
-- (void) savePendingAccount:(DCTAuthAccount *) account
-{
-  _account = account;
-}
-
-- (void) clearPendingAccount
-{
-    NSLog(@"called clearPendingAccount: %@", _account);
-    [_account cancelAuthentication];
-    _account = nil;
-}
-
-- (void (^)(DCTAuthResponse *response, NSError *error)) getHandler:(DCTAuthAccount *) account
-                                                         onSuccess:(AuthManagerCompletionBlock) onSuccess
-                                                           onError:(AuthManagerErrorBlock) onError
-{
-    return ^(DCTAuthResponse *response, NSError *error) {
+    DCTOAuth2Account *account = [self getAccount:providerName cfg:cfg];
+    [account reauthenticateWithHandler:^(DCTAuthResponse *response, NSError *error) {
         NSLog(@"Reauthenticating...");
         if (error != nil) {
-            onError(error);
+            NSError *err = [NSError errorWithDomain:error.domain
+                                               code:error.code
+                                           userInfo:@{@"response": response.responseDescription}];
+            onError(err);
             return;
         }
         
@@ -71,8 +81,53 @@
         }
         
         onSuccess(account);
-    };
+    }];
 }
 
+#pragma mark - private
+
+- (DCTOAuth2Account *) getAccount:(NSString *)providerName
+                              cfg:(NSDictionary *)cfg
+{
+    DCTOAuth2Account *account;
+    // Required
+    NSURL *authorize_url = [cfg objectForKey:@"authorize_url"];
+    NSString *scopeStr = [cfg valueForKey:@"scopes"];
+    // NSArray *scopes = [scopeStr componentsSeparatedByString:@","];
+
+    NSString *sep = @" ";
+    NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:sep];
+    NSArray *scopes = [scopeStr componentsSeparatedByCharactersInSet:set];
+
+    // Optional
+    
+    NSURL *access_token_url = [cfg objectForKey:@"access_token_url"];
+    NSString *clientID = [cfg valueForKey:@"client_id"];
+    NSString *clientSecret = [cfg valueForKey:@"client_secret"];
+    NSString *username = [cfg valueForKey:@"username"];
+    NSString *password = [cfg valueForKey:@"password"];
+    
+    NSLog(@"getAccount config: %@", cfg);
+    if (access_token_url != nil) {
+        account = [[DCTOAuth2Account alloc] initWithType:providerName
+                                            authorizeURL:authorize_url
+                                          accessTokenURL:access_token_url
+                                                clientID:clientID
+                                            clientSecret:clientSecret
+                                                  scopes:scopes];
+    } else {
+        account = [[DCTOAuth2Account alloc] initWithType:providerName
+                                            authorizeURL:authorize_url
+                                                clientID:clientID
+                                            clientSecret:clientSecret
+                                                username:username
+                                                password:password
+                                                  scopes:scopes];
+    }
+
+    [self savePendingAccount:account];
+    
+    return account;
+}
 
 @end
